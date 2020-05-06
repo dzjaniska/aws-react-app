7c7e688b046815592de5d4c09f0d71e625d0c8a2
divanou <divanou@scnsoft.com>
2020-05-06 11:43:37 +0300

    package.json homepage property set

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,44 +1,42 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-import {makeStyles} from '@material-ui/styles';
-import {Box} from '@material-ui/core';
-import PerfectScrollbar from 'react-perfect-scrollbar';
+import React, {useState} from "react";
+import PropTypes from "prop-types";
+import {makeStyles} from "@material-ui/styles";
+import {Box} from "@material-ui/core";
+import PerfectScrollbar from "react-perfect-scrollbar";
 import TopBar from "./topBar/TopBar";
 import NavBar from "./navBar/NavBar";
 import navigationConfig from "../../navigationConfig";
 
 const useStyles = makeStyles(theme => ({
     root: {
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        overflow: 'hidden'
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        overflow: "hidden"
     },
     topBar: {
         zIndex: 2,
-        position: 'relative'
+        position: "relative"
     },
     container: {
-        display: 'flex',
-        flex: '1 1 auto',
-        overflow: 'hidden',
-        borderTop: '1px solid ' + theme.palette.divider
+        display: "flex",
+        flex: "1 1 auto",
+        overflow: "hidden",
+        borderTop: "1px solid " + theme.palette.divider
     },
     navBar: {
         zIndex: 3,
         width: 230,
         minWidth: 230,
-        flex: '0 0 auto',
+        flex: "0 0 auto",
         backgroundColor: theme.palette.background.main || theme.palette.primary.main,
-        '@media (max-width: 1279px)': {
-            width: '100%'
+        "@media (max-width: 1279px)": {
+            width: "100%"
         }
     },
     content: {
-        overflowY: 'auto',
-        flex: '1 1 auto',
-        '-webkit-overflow-scrolling': 'touch'
+        flex: '1 1 auto'
     }
 }));
 
@@ -61,9 +59,7 @@ const Dashboard = props => {
                         user={user}
                 />
                 <main className={classes.content}>
-                    <PerfectScrollbar>
                         {props.children}
-                    </PerfectScrollbar>
                 </main>
             </Box>
         </Box>
